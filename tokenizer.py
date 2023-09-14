cbdac749261bd3cda071d9d4262647cf37678f78
nelly <dldyddn0624@gmail.com>
2023-09-15 01:05:07 +0900

    torch.no_grad() in tokenizer model.

--- tokenizer.py
+++ tokenizer.py
@@ -12,12 +12,14 @@ def tokenize(self, texts: list[str]) -> dict:
         return_tensors="pt",  # return pytorch tensor
         truncation=True,
         max_length=self.max_length,
-        padding="max_length"
+        padding="max_length",
     )
     return tokens
 
 
-def texts_to_embeddings(texts: list[str], max_length: int = 20) -> tuple[torch.Tensor, torch.Tensor]:
+def texts_to_embeddings(
+    texts: list[str], max_length: int = 20
+) -> tuple[torch.Tensor, torch.Tensor]:
     """Text 를 입력 받아서 임베딩 벡터로 변환."""
     tokens = TOKENIZER(
         texts,
@@ -26,18 +28,15 @@ def texts_to_embeddings(texts: list[str], max_length: int = 20) -> tuple[torch.T
         max_length=max_length,
         padding="max_length",
     )
-    outputs = MODEL(**tokens)
+    with torch.no_grad():
+        outputs = MODEL(**tokens)
     embeddings = outputs.last_hidden_state
-    masks = tokens['attention_mask']
+    masks = tokens["attention_mask"]
     return embeddings, masks
 
 
 if __name__ == "__main__":
-    embeddings, masks = texts_to_embeddings(
-        [
-            "이것은 테스트 문장 입니다.",
-            "이것은 십새키 입니다.",
-        ]
-    )
+    texts = ["이것은 테스트 문장 입니다.", "이것은 십새키 입니다."]
+    embeddings, masks = texts_to_embeddings()
     print(embeddings.shape, masks.shape)
     # torch.Size([2, 20, 768]) torch.Size([2, 20])
